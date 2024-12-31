fcf274e79ef6b7397b0200262d7474e521c46290
pwq <1066784440@qq.com>
2024-12-31 11:59:47 +0800

    test if the compiler is suits the real bits of my computer.

--- test/test.c
+++ test/test.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("hi pwq, you back finally.");
+    printf("hi pwq, you back finally.\n");
+
+    int BytesOfint = 0;
+    printf("In my compute, there is %d bytes composing int.\n", sizeof(BytesOfint));
+    long int BytesOfLongint = 0;
+    printf("In my compute, there is %d bytes composing long int.\n", sizeof(BytesOfLongint));
     return 0;
 }
